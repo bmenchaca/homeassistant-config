04c245e301bedc4201d1f59346aa97102ab87cc4
Ben Menchaca <ben.menchaca@gmail.com>
2018-05-20 11:25:02 -0500

    Moved appdaemon to it's own repo.

--- appdaemon/apps/hello.py
+++
@@ -1,8 +0,0 @@
-import appdaemon.plugins.hass.hassapi as hass
-
-class HelloWorld(hass.Hass):
-
-    def initialize(self):
-        self.log("Hello from Appdaemon")
-        self.log("You are now ready to run apps!")
-
